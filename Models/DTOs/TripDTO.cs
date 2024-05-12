7fc2c671e873397c031b7daedb0be90baac7009b
lostmyvirginity <jandab6@tlen.pl>
2024-05-12 18:27:55 +0200

    Initial commit

---
+++ Models/DTOs/TripDTO.cs
@@ -0,0 +1,9 @@
+namespace Zadanie7.Models.DTOs;
+
+public class TripDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DateFrom { get; set; }
+    public DateTime 
+}
